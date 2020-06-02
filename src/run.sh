82262e7ca8c078e124c919dafcf40a1003edf827
pan <1094170587@qq.com>
2020-06-02 21:00:38 +0800

    添加可视化输出

--- src/run.sh
+++ src/run.sh
@@ -1,9 +1,14 @@
-#训练
+#训练coco
 # python main.py ctdet --exp_id coco_dla --batch_size 16 --master_batch 15 --lr 1.25e-4  --gpus 0
 
+#训练自定义数据
 # python main.py ctdet --exp_id pascal_dla_384 --dataset pascal --num_epochs 70 --lr_step 45,60 --batch_size 24
 
-#predict
-# python demo.py ctdet --demo /input0/test2017/000000097335.jpg --load_model /output/ctdet_coco_dla_2x.pth
+#predict 有gt
+#python demo.py ctdet --demo val_mask.csv --load_model /output/tf_dir/ctdet/pascal_dla_384/model_best.pth
 
-python demo.py ctdet --demo /input1/images/flir_20191211T124157_rgb_image.jpg --load_model /output/tf_dir/ctdet/pascal_dla_384/model_best.pth
+#predict 无gt
+python predict.py ctdet --demo val_mask.csv --load_model /output/tf_dir/ctdet/pascal_dla_384/model_best.pth
+
+#计算F1 score
+# python pascalvoc.py  --gtfolder /input0/ --detfolder ./ --threshold 0.7
